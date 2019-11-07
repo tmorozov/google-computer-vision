f7414892d3f47564e436bd6a3c5b22403fcf0895
Tymur Morozov <tmorozov@lohika.com>
2019-11-07 14:20:52 +0200

    server with google api

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+
 const path = require('path');
+const util = require('util');
+const fs = require('fs');
 const express = require('express');
+const multer = require('multer');
+const { ImageAnnotatorClient } = require('@google-cloud/vision');
+
+const readdirAsync = util.promisify(fs.readdir);
+const statAsync = util.promisify(fs.stat);
+const unlinkAsync = util.promisify(fs.unlink);
+
+fs.writeFileSync(path.join(__dirname, 'gcloud-credentials.json'), process.env.SERVICE_ACCOUNT_JSON);
+// create Cloud Vision client
+const visionClient = new ImageAnnotatorClient();
+
 const app = express();
 app.use(express.static(path.join(__dirname, '../client/build')));
 
